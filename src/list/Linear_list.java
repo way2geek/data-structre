a0872b6bddd8ac1cdaa98eb2922e0090cdf903ae
way2geek <249057169@qq.com>
2020-04-05 15:27:03 +0800

    bug fix

--- src/list/Linear_list.java
+++ src/list/Linear_list.java
@@ -10,6 +10,24 @@ public class Linear_list implements List {
 		listElem = new Object[size];
 	}
 
+	public static void merge(Linear_list list1, Linear_list list2) {
+		Linear_list list = new Linear_list(list1.curLen + list2.curLen);
+//		list.listElem[list1.curLen]= list1.listElem[list1.curLen];
+//		list.curLen=list1.curLen;
+//		list.display();
+		for (int i = 0; i < list1.curLen; i++) {
+			list.insert(i, list1.listElem[i]);
+		}
+		// list2.display();
+		for (int i = 0; i < list2.curLen; i++) {
+			if (list.indexOf(list2.listElem[i]) == -1) {
+				// System.out.println(list.indexOf(list2.listElem[i]));
+				list.insert(list.curLen, list2.listElem[i]);
+			}
+		}
+		list.display();
+	}
+
 	@Override
 	public void clear() {
 		for (int i = 0; i <= curLen; i++) {
@@ -42,20 +60,19 @@ public class Linear_list implements List {
 	@Override
 	public void insert(int i, Object x) {
 		if (i <= curLen) {
-			if (curLen==0) {
+			if (curLen == 0||i==curLen) {
 				listElem[i] = x;
 				curLen++;
-			}else {
-			for (int j = curLen-i+1;j>0 ; j--) {
-				listElem[j+i-1] = listElem[j+i-2];
+			} else {
+				for (int j = curLen - i + 1; j > 0; j--) {
+					listElem[j + i - 1] = listElem[j + i - 2];
+				}
+				listElem[i] = x;
+				curLen++;
 			}
-			listElem[i] = x;
-			curLen++;
-		}} else {
+		} else {
 			System.out.print("invalid index");
 		}
-
-		
 
 	}
 
@@ -68,12 +85,13 @@ public class Linear_list implements List {
 	}
 
 	@Override
-	public void indexOf(Object o) {
+	public int indexOf(Object o) {
 		for (int i = 0; i < curLen; i++) {
-			if (listElem[i].equals(o)) {
-				System.out.print(i);
+			if (this.listElem[i].equals(o)) {
+				return i;
 			}
 		}
+		return -1;
 
 	}
 
@@ -85,17 +103,21 @@ public class Linear_list implements List {
 	}
 
 	public static void main(String args[]) {
-		Linear_list l = new Linear_list(100);
-		l.insert(0, "123");
-		l.insert(1, "1234");
-		l.insert(1, "12345");
-		l.insert(1, "1234567");
-		l.insert(1, "12345678");
-		l.remove(1);
-		l.display();
-		l.indexOf("12345678");
-		l.clear();
-		l.display();
+		Linear_list l1 = new Linear_list(100);
+		Linear_list l2 = new Linear_list(100);
+		l1.insert(0, "123");
+		l1.insert(1, "1234");
+		l1.insert(1, "12345");
+		l1.insert(1, "1234567");
+		l1.insert(1, "12345678");
+		l2.insert(0, "321");
+		l2.insert(1, "3210");
+		l2.insert(1, "12345");
+		l2.insert(1, "12333");
+		l2.insert(1, "1234678");
+		l2.insert(5, "1234678");
+		merge(l1, l2);
+
 	}
 
 }
--- src/list/List.java
+++ src/list/List.java
@@ -7,6 +7,6 @@ public interface List {
 	public Object get(int i);
 	public void insert(int i ,Object x);
 	public void  remove (int i );
-	public  void indexOf(Object o);
+	public  int indexOf(Object o);
 	public void display ();
 } 
